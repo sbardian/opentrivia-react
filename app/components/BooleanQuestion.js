8ef5196fbbbfd12c7744c059ad7323024be29426
Brian <sbardian@gmail.com>
2017-05-18 19:22:38 -0600

    Add comments on what to use for this component.

--- app/components/BooleanQuestion.js
+++ app/components/BooleanQuestion.js
@@ -7,6 +7,7 @@ import React from 'react';
 export default class BooleanQuestion extends React.Component {
   render () {
     // TODO: take props and render true false question
+    // make use of react-native-flip-card
     return (
         <li>
 
