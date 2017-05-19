2154c22ae0f5ef340defb4fb48949f9ea565ca36
Brian <sbardian@gmail.com>
2017-05-18 19:23:42 -0600

    Base Component work.

--- app/index.js
+++ app/index.js
@@ -4,34 +4,80 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import BooleanQuestion from 'components/BooleanQuestion';
-import MultipleQuestionWrapper from 'components/MultipleQuestionWrapper';
-import MultipleQuestionChoice from 'components/MultipleQuestionChoice';
+import utils from '../utils/utils';
 
-export default class openTrivia extends React.Component {
+export default class OpenTrivia extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
       loading: true,
+      results: [{}],
+      index: 0,
     }
+  }
+
+  nextQuestion () {
+    this.setState({
+      index: this.state.index++
+    })
+  }
+
+  prevQuestion () {
+    this.setState({
+      index: this.state.index--
+    })
+  }
+
+  componentWillMount () {
+    const {
+      loading,
+      amount,
+      category,
+      difficulty,
+      type,
+      encode,
+      getToken
+    } = this.props;
+    const options = {
+      amount: amount,
+      category: category,
+      difficulty: difficulty,
+      type: type,
+      encode: encode,
+      getToken: getToken,
+    }
+    utils.getQuestions(options)
+        .then((results) => {
+          console.log('index results ', results);
+          this.setState({ results })
+        });
   }
 
   render () {
     // TODO: foreach question, if multi or bool call multi wrapper or bool question
     return (
         <div>
-
+          <h1>{this.state.results[this.state.index].category}</h1>
+          <h2>{this.state.results[this.state.index].question}</h2>
+          <h2>Correct: {this.state.results[this.state.index].correct_answer}</h2>
         </div>
     )
   }
 }
 
-openTrivia.propTypes = {
+OpenTrivia.propTypes = {
   // set default props the component will expect
-  loading: PropTypes.bool
+  loading: PropTypes.bool,
+  amount: PropTypes.number,
+  category: PropTypes.number,
+  difficulty: PropTypes.string,
+  type: PropTypes.string,
+  encode: PropTypes.string,
+  getToken: PropTypes.bool,
 };
 
-openTrivia.defaultProps = {
+OpenTrivia.defaultProps = {
   // set value of default props
   loading: true,
+  results: [{}],
 };
