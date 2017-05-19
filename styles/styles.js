cabcc0df94d236116e746013dfe1fa4d5d7936a2
Brian <sbardian@gmail.com>
2017-05-18 22:26:20 -0600

    Update card width.

---
+++ styles/styles.js
@@ -0,0 +1,21 @@
+/**
+ * Created by sbardian on 5/18/17.
+ */
+
+var styles = {
+  example: {
+    height: '100%',
+    width: '400px'
+  },
+  card: {
+    border: '1px solid #eeeeee',
+    borderRadius: '3px',
+    padding: '15px',
+    width: '400px'
+  },
+  image: {
+    width: '250px'
+  }
+}
+
+module.exports = styles;
