9d91c73f9032b648dc468e55658fdcf3c6970a7c
Brian <sbardian@gmail.com>
2017-05-19 18:48:35 -0600

    Logic for only getting a token onces. .  more to come.

--- utils/utils.js
+++ utils/utils.js
@@ -3,21 +3,27 @@
  */
 
 const openTriviaAPI = require('opentriviaapi');
+let token = null;
 
 const utils = {
   getToken() {
     return openTriviaAPI.getToken();
   },
   getQuestions(options) {
-    if (options.getToken) {
+    if (options.getToken && !token) {
       utils.getToken()
           .then((data) => {
+            token = data.token;
             options.token = data.token;
           });
       return openTriviaAPI.getQuestions(options)
           .then((data) => {
             return data.results;
           });
+      // TODO: deal with token needing reset.
+    } else {
+      options.token = token;
+      return openTriviaAPI.getQuestions(options);
     }
   }
 };
