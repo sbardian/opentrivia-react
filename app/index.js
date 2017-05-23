55eade74f36d2b3e50bc408df842b9e41a139c5f
Brian <sbardian@gmail.com>
2017-05-23 09:51:55 -0600

    Refactor and add spread support.

--- app/index.js
+++ app/index.js
@@ -4,11 +4,11 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import utils from '../utils/utils';
+import openTriviaAPI from 'opentriviaapi';
 import QuestionWrapper from './components/QuestionWrapper';
 
 export default class OpenTrivia extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       loading: true,
@@ -18,10 +18,11 @@ export default class OpenTrivia extends React.Component {
     }
     this.nextQuestion = this.nextQuestion.bind(this);
     this.prevQuestion = this.prevQuestion.bind(this);
+    this.getToken = this.getToken.bind(this);
   }
 
   // Moves forward to the next question
-  nextQuestion () {
+  nextQuestion() {
     console.log('NEXT');
     let i = this.state.index;
     if (i < (this.props.amount - 1)) {
@@ -33,7 +34,7 @@ export default class OpenTrivia extends React.Component {
   }
 
   // Moves backwards to the previous question
-  prevQuestion () {
+  prevQuestion() {
     console.log('PREV');
     let i = this.state.index;
     if (i > 0) {
@@ -44,58 +45,41 @@ export default class OpenTrivia extends React.Component {
     });
   }
 
-  componentWillMount () {
-    const {
-      amount,
-      category,
-      difficulty,
-      type,
-      encode,
-      getToken
-    } = this.props;
-    const options = {
-      amount: amount,
-      category: category,
-      difficulty: difficulty,
-      type: type,
-      encode: encode,
-      getToken: getToken,
+  getToken() {
+    if (this.props.getToken && !this.state.token) {
+      return openTriviaAPI.getToken()
+          .then(data => data.token);
     }
-    if(options.getToken && !this.state.token) {
-      utils.getToken()
-          .then((data) => {
-            this.setState({
-              token: data.token,
-            });
-            options.token = data.token;
-            utils.getQuestions(options)
-                .then((data) => {
-                  this.setState({
-                    results: data.results,
-                    loading: false,
-                  });
-                });
-          });
-    } else {
-      utils.getQuestions(options)
-          .then((data) => {
-              this.setState({
-                results: data.results,
-                loading: false,
-              });
-            });
-    }
+    return this.state.token;
   }
 
-  render () {
+  componentWillMount() {
+    this.getToken()
+        .then((token) => {
+          this.setState({token});
+          return openTriviaAPI.getQuestions({token, ...this.props});
+        })
+        .then((data) => {
+          this.setState({
+            results: data.results,
+            loading: false,
+          });
+        });
+  }
+
+  render() {
     return this.state.loading === true
         ? <div><h1>Loading Question. . .</h1></div>
         : <div>
           <h4>{(this.state.index + 1)} / {this.props.amount}</h4>
-          <div><button onClick={this.nextQuestion}>Next</button></div>
-          <div><button onClick={this.prevQuestion}>Prev</button></div>
-          <QuestionWrapper question={this.state.results[this.state.index]} />
+          <div>
+            <button onClick={this.nextQuestion}>Next</button>
           </div>
+          <div>
+            <button onClick={this.prevQuestion}>Prev</button>
+          </div>
+          <QuestionWrapper question={this.state.results[this.state.index]}/>
+        </div>
   }
 }
 
