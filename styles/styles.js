8b195240c3ea9450da9f83ce02866fd53e3c8518
Brian <sbardian@gmail.com>
2017-05-24 20:57:53 -0600

    Add ul styles.

--- styles/styles.js
+++ styles/styles.js
@@ -5,7 +5,8 @@
 var styles = {
   example: {
     height: '100%',
-    width: '400px'
+    width: '400px',
+    margin: '0 auto',
   },
   card: {
     border: '1px solid #666',
@@ -15,8 +16,12 @@ var styles = {
     backgroundColor: 'lightcyan',
     color: '#666',
   },
+  cardList: {
+    listStyle: 'none',
+    padding: '0',
+  },
   image: {
-    width: '250px'
+    width: '250px',
   }
 }
 
