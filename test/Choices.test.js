67a2676c5dd47a2a7b80a6963e128c88705ea803
Brian <sbardian@gmail.com>
2017-05-24 20:02:47 -0600

    Initial testing env setup.

---
+++ test/Choices.test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sbardian on 5/17/17.
+ */
+
+import React from 'react'
+import {render, mount, shallow} from 'enzyme'
+import {expect} from 'chai'
+
+import Choices from '../app/components/Choices';
+
+describe('Testing Choices...', () => {
+  it('Should render a ul element', (done) => {
+    const wrapper = render(<Choices />);
+    expect(wrapper.find('ul')).to.have.length(1);
+    done();
+  });
+});
