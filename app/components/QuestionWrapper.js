240bc3bf5a2411c5142ef3e73ffb759d4c44e2ac
Brian <sbardian@gmail.com>
2017-05-25 19:36:35 -0600

    Refactor.

--- app/components/QuestionWrapper.js
+++ app/components/QuestionWrapper.js
@@ -24,8 +24,7 @@ export default class QuestionWrapper extends React.Component {
   }
 
   htmlDecode (input) {
-    let e = document.createElement('div');
-    e.innerHTML = input;
+    let e = document.createElement('div').innerHTML = input;
     return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
   }
 
